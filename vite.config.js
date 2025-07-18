ba567b87776156e58a00f899712901c4ccb778f9
brendonmuneatgmaildotcom <68879038+brendonmuneatgmaildotcom@users.noreply.github.com>
2025-07-19 11:50:29 +1200

    Force clean build with updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',        // Default, but explicitly declared
+    emptyOutDir: true,     // Ensures the output folder is cleared before each build
+  },
+});
